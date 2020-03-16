2c1566b8a6dc417df8ebe277f511bc8f67faaa2e
AndriiHryhorovskyi <private_ukr_pochta@ukr.net>
2020-03-16 14:46:18 +0200

    fix: fix sys controller

--- src/controllers/sys.js
+++ src/controllers/sys.js
@@ -1,26 +1,30 @@
 const { hello, id } = require("../BL");
 
 async function sayHello(req, res, next) {
-  const data = { data: "" };
+  const resData = { data: "" };
   // catch errors
   try {
     const result = await hello();
     // catch exceptions
     if (result instanceof Error) return res.status(418).send(result.message);
-    return res.status(200).json(data);
+
+    resData.data = result;
+    return res.status(200).json(resData);
   } catch (error) {
     return next(error);
   }
 }
 
 async function echo(req, res, next) {
-  const data = { data: undefined };
+  const resData = { data: undefined };
   // catch errors
   try {
     const result = await id(req.body);
     // catch exceptions
     if (result instanceof Error) return res.status(418).send(result.message);
-    return res.json(data);
+
+    res.data = result;
+    return res.json(resData);
   } catch (error) {
     return next(error);
   }
