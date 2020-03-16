95dec37cfc7ff71467c45ae49b0cf5410f170678
AndriiHryhorovskyi <private_ukr_pochta@ukr.net>
2020-03-16 09:47:02 +0200

    chore: change exported object from actors

--- src/actors/index.js
+++ src/actors/index.js
@@ -1,3 +1,14 @@
-module.exports = {
-  logger: console
-}
+const { importAll } = require("../lib");
+
+const actors = importAll(__dirname);
+const actorsNames = Object.keys(actors);
+
+const actorsInstances = Object.entries(actors).reduce(
+  (obj, [key, value]) => ((obj[key] = value.instance), obj),
+  {},
+);
+
+const exitAll = () =>
+  Promise.allSettled(actorsNames.map(actorName => actors[actorName].exit()));
+
+module.exports = { ...actorsInstances, exitAll };
