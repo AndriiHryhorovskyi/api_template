8fb86a23cab4ea73adc6344e5af7e4d6e96a680b
AndriiHryhorovskyi <private_ukr_pochta@ukr.net>
2020-03-16 19:26:51 +0200

    chore: add config module

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,13 @@
-module.exports = {
-  PORT: 3000,
+const rc = require("rc");
+
+const PORT = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || "development";
+
+module.exports = rc("res_api_template", {
+  PORT,
+  WEBHOST: `http://127.0.0.1:${PORT}`,
   ALLOW_ORIGINS: "*",
+  IS_DEV: env === "development",
   SHUTDOWN_TIMEOUT: 3000,
-  REFRESH_TIMEOUT: 5 
-};
+  TREFRESH_TIMEOUT: 5,
+});
